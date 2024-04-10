04d34184242d845e22b293562d873b167854263a
Annabelle Perbet <annabelleperbet@yahoo.fr>
2024-04-10 19:10:22 +0200

    Ajout du fichier script.js contenant la logique de la page web

---
+++ script.js
@@ -0,0 +1,43 @@
+// Récupérartion des éléments
+const select = document.querySelector("select");
+const list = document.querySelector("ul");
+const h1 = document.querySelector("h1");
+
+// Ecoute de l'événement change sur le select
+select.addEventListener("change", () => {
+  // Création de la constante choice
+  const choice = select.value;
+  // Création de la variable days régler à 31 de base
+  let days = 31;
+
+  if (choice === "Février") {
+    days = 28;
+  } else if (
+    choice === "Avril" ||
+    choice === "Juin" ||
+    choice === "Septembre" ||
+    choice === "Novembre"
+  ) {
+    days = 30;
+  } else {
+    days = 31;
+  }
+
+  // Appel de la fonction createCalendar ayant pour paramètres le nombre de jour (days) et le choix de l'utilisateur (choice)
+  createCalendar(days, choice);
+});
+
+// Création de la fonction createCalendar ayant pour paramètres le nombre de jour (days) et le choix de l'utilisateur (choice)
+const createCalendar = (days, choice) => {
+  list.textContent = "";
+  h1.textContent = choice;
+  for (let i = 1; i <= days; i++) {
+    // Création d'un élément liste et insertion dans le DOM
+    const listItem = document.createElement("li");
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+};
+
+// Appel de la fonction CreateCalendar
+createCalendar(31, "Janvier");
